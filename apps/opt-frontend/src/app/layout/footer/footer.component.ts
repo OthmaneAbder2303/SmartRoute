d890dc441d894d7fc53c94a85d9544e657e1f429
Othmane Abderrazik <othmane232004@gmail.com>
2025-03-24 23:31:33 +0000

    navbar - footer

---
+++ apps/opt-frontend/src/app/layout/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+
+@Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule, FaIconComponent],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
+})
+export class FooterComponent {}
--- apps/opt-frontend/tailwind.config.js
+++ apps/opt-frontend/tailwind.config.js
@@ -7,8 +7,9 @@ module.exports = {
     join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
     ...createGlobPatternsForDependencies(__dirname),
   ],
+  // media: false,
   theme: {
-    fontfamily: {
+    fontFamily: {
       'sans': 'Inter var, ui-sans-serif, system-ui',
       'serif': 'Inter var, ui-sans-serif, system-ui',
     },
@@ -26,11 +27,11 @@ module.exports = {
   daisyui: {
     themes: [
       {
-        'fantasy': {
-          primary: '#0000FF',
+        fantasy: {
+          primary: '#0000ff',
           'primary-content': 'white',
-          secondary: '#F6F6F6',
-          neutral: '#E8E8E8',
+          secondary: '#f6f6f6',
+          neutral: '#e8e8e8',
         },
       },
     ],
@@ -39,7 +40,7 @@ module.exports = {
     utils: true,
     prefix: '',
     logs: true,
-    themeRoot: 'root'
+    themeRoot: ':root',
   },
   plugins: [
     require('@tailwindcss/typography'),
