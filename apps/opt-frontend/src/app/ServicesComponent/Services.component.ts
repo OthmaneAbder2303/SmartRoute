aed9ad91d02f6076b9707b8b71877449baf4e118
zakariaayl <ayougilzaki@gmail.com>
2025-03-31 07:39:25 +0200

    configure services route(not finished)

---
+++ apps/opt-frontend/src/app/ServicesComponent/Services.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-services',
+  imports: [CommonModule],
+  templateUrl: './Services.component.html',
+  styleUrl: './Services.component.scss',
+})
+export class ServicesComponent {
+  usemap() {
+
+  }
+}
--- apps/opt-frontend/src/app/app.routes.ts
+++ apps/opt-frontend/src/app/app.routes.ts
@@ -3,16 +3,17 @@ import { HeroComponent } from './hero/hero.component';
 import { PricingComponent } from './pricing/pricing.component';
 import {CallbackComponent} from "./CallbackComponent/callback.component";
 import {AppComponent} from "./app.component";
+import {ServicesComponent} from "./ServicesComponent/Services.component";
 
 export const appRoutes: Route[] = [
   {
     path: '',
-    component: AppComponent,
+    component: HeroComponent,
   },
   // {
   //   path: 'pricing',
   //   component: PricingComponent,
   // },
-  { path: 'login/callback', component: CallbackComponent }
-
+  { path: 'login/callback', component: CallbackComponent },
+  { path: 'services', component: ServicesComponent }
 ];
