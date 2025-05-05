51f48fbde40853f3e9d1bb117b2f416102b35c59
Othmane Abderrazik <othmane232004@gmail.com>
2025-05-05 20:33:44 +0100

    let the map cover all screen

--- apps/opt-frontend/src/app/components/map/map.component.ts
+++ apps/opt-frontend/src/app/components/map/map.component.ts
@@ -1,8 +1,9 @@
-import { Component, AfterViewInit, OnDestroy, Inject, PLATFORM_ID } from '@angular/core';
+import { Component, AfterViewInit, OnDestroy } from '@angular/core';
+import * as L from 'leaflet';
 import { HttpClient } from '@angular/common/http';
-import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-map',
@@ -12,77 +13,48 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements AfterViewInit, OnDestroy {
-  places = [
+  places =[
     {name: 'Jemaa el-Fnaa', lat: 31.625, lng: -7.989},
     {name: 'Menara Gardens', lat: 31.616, lng: -8.012},
     {name: 'Majorelle Garden', lat: 31.637, lng: -8.002},
   ];
-  
+
   startPlace: any;
   endPlace: any;
-  
-  map: any;
+
+  map: L.Map | undefined;
   startMarker: any;
   endMarker: any;
   routeLine: any;
-  
-  customIcon: any;
-  L: any; // Leaflet instance
-  isBrowser: boolean;
-  
-  constructor(
-    private http: HttpClient,
-    @Inject(PLATFORM_ID) private platformId: Object
-  ) {
-    this.isBrowser = isPlatformBrowser(this.platformId);
-  }
-  
-  ngAfterViewInit(): void {
-    // Only initialize map in browser environment
-    if (this.isBrowser) {
-      this.initMap();
-    }
-  }
-  
-  private initMap(): void {
-    // Dynamically import Leaflet only in browser
-    import('leaflet').then((L) => {
-      this.L = L;
-      
-      // Initialize map
-      this.map = L.map('map').setView([31.63, -7.99], 13);
-      
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: 'Map data © OpenStreetMap contributors'
-      }).addTo(this.map);
-      
-      // Initialize custom icon
-      this.customIcon = L.icon({
-        iconUrl: './assets/map-marker-end.png',
-        iconSize: [32, 32],
-        iconAnchor: [16, 32],
-        popupAnchor: [0, -32]
-      });
-      
-      this.map.on('click', (e: any) => this.handleMapClick(e));
-    }).catch(error => {
-      console.error('Error loading Leaflet:', error);
+
+  customIcon: L.Icon;
+
+  constructor(private http: HttpClient) {
+    this.customIcon = L.icon({
+      iconUrl: './assets/map-marker-end.png',  
+      iconSize: [32, 32],                  
+      iconAnchor: [16, 32],              
+      popupAnchor: [0, -32]               
     });
   }
-  
-  ngOnDestroy(): void {
-    if (this.isBrowser && this.map) {
+  ngAfterViewInit(): void {
+    this.map = L.map('map').setView([31.63, -7.99], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data © OpenStreetMap contributors'
+    }).addTo(this.map);
+    this.map.on('click', (e: any) => this.handleMapClick(e));
+  }
+  ngOnDestroy(): void{
+    if (this.map) {
       this.map.remove();
     }
   }
-  
-  handleMapClick(e: any) {
+  handleMapClick(e: any){
     if (!this.map) return;
-    
-    if (!this.startMarker) {
-      this.startMarker = this.L.marker(e.latlng, { icon: this.customIcon }).addTo(this.map);
-    } else if (!this.endMarker) {
-      this.endMarker = this.L.marker(e.latlng, { icon: this.customIcon }).addTo(this.map);
+    if (!this.startMarker){
+      this.startMarker = L.marker(e.latlng, { icon: this.customIcon }).addTo(this.map);
+    } else if (!this.endMarker){
+      this.endMarker = L.marker(e.latlng, { icon: this.customIcon }).addTo(this.map);
       this.requestRoute();
     } else {
       this.map.removeLayer(this.startMarker);
@@ -91,40 +63,32 @@ export class MapComponent implements AfterViewInit, OnDestroy {
       this.startMarker = this.endMarker = this.routeLine = null;
     }
   }
-  
+
   onSelectChange() {
-    if (!this.isBrowser) return;
-    
     if (this.startPlace && this.endPlace) {
       this.setMapMarkersAndRoute(this.startPlace, this.endPlace);
     }
   }
-  
+
   setMapMarkersAndRoute(start: any, end: any) {
-    if (!this.isBrowser || !this.map) return;
-    
-    if (this.startMarker) this.map.removeLayer(this.startMarker);
-    if (this.endMarker) this.map.removeLayer(this.endMarker);
-    
-    this.startMarker = this.L.marker([start.lat, start.lng], { icon: this.customIcon }).addTo(this.map);
-    this.endMarker = this.L.marker([end.lat, end.lng], { icon: this.customIcon }).addTo(this.map);
-    
+    if (this.startMarker) this.map?.removeLayer(this.startMarker);
+    if (this.endMarker) this.map?.removeLayer(this.endMarker);
+
+    this.startMarker = L.marker([start.lat, start.lng], { icon: this.customIcon }).addTo(this.map!);
+    this.endMarker = L.marker([end.lat, end.lng], {icon: this.customIcon}).addTo(this.map!);
+
     this.requestRoute();
   }
-  
+
   requestRoute() {
-    if (!this.isBrowser) return;
-    
     const start = this.startMarker.getLatLng();
     const end = this.endMarker.getLatLng();
-    
-    this.http.post<any>('http://localhost:8080/predict/testMap', {}).subscribe(response => {
+    this.http.post<any>('http://localhost:8080/predict/testMap', {}).subscribe(response =>{
       console.log(response);
       const latlngs = response.route.map((p: any) => [p.lat, p.lng]);
-      
-      if (this.routeLine) this.map.removeLayer(this.routeLine);
-      this.routeLine = this.L.polyline(latlngs, { color: 'blue' }).addTo(this.map);
-      this.map.fitBounds(this.routeLine.getBounds());
+      if (this.routeLine) this.map?.removeLayer(this.routeLine);
+      this.routeLine = L.polyline(latlngs, { color: 'blue' }).addTo(this.map!);
+      this.map?.fitBounds(this.routeLine.getBounds());
     });
   }
 }
